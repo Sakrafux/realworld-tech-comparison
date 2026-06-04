bddde45f43c65f4f79830eaf053e1cd5c88948eb
Andreas Hell <andhell03@gmail.com>
2026-06-04 18:49:25 +0200

    Astro: Switched to Preact

--- frontend/astro/astro.config.mjs
+++ frontend/astro/astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3000 },
-  integrations: [react()]
+    server: { port: 3000 },
+    integrations: [preact()],
 });
